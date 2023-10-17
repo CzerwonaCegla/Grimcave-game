e4e1a14da8006fdafadc0cf29d5353ec9286f76b
Stanisław Sklepowicz <stanislaw.sklepowicz@gmail.com>
2023-10-17 20:49:31 +0200

    Mały update do movementu gracza

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Vector2 moveDir, jumpDir;
-    [SerializeField] float movementSpeed = 5;
-    [SerializeField] float jumpForce = 10;
+    [SerializeField] float movementSpeed = 5f;
+    [SerializeField] float jumpForce = 10f;
+    [SerializeField] float dashDistance = 15f;
     Rigidbody2D rb;
+    private bool isDashing;
 
     void Start()
     {
@@ -17,23 +19,31 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         Movement();
+        Jump();
     }
 
     private void Movement()
     {
+        //Move left
         if (Input.GetKey(KeyCode.A))
         {
             moveDir = new Vector2(-1, 0);
             transform.Translate(moveDir * Time.deltaTime * movementSpeed);
         }
+        //Move right
         if (Input.GetKey(KeyCode.D))
         {
             moveDir = new Vector2(1, 0);
             transform.Translate(moveDir * Time.deltaTime * movementSpeed);
         }
+    }
+
+    void Jump()
+    {
+        //Jump
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            if (rb.velocity.y == 0) 
+            if (rb.velocity.y == 0)
             {
                 jumpDir = new Vector2(0, 1);
                 rb.AddForce(jumpDir * jumpForce, ForceMode2D.Impulse);
