748592a5e4eb01eaee3ea19248de29bc42774459
Stanisław Sklepowicz <stanislaw.sklepowicz@gmail.com>
2023-10-19 22:48:32 +0200

    Start skryptu do kamery

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private GameObject cam;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
