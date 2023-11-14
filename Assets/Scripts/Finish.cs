be69ed6c9eee0367a9c874dc3cfd13eb998694a5
Stanisław Sklepowicz <stanislaw.sklepowicz@gmail.com>
2023-11-15 00:34:55 +0100

    Mała optymalizacja checku zmiany sceny

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -38,7 +38,7 @@ public class Finish : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player" & sceneIndex <= totalSceneCount)
+        if (collision.tag == "Player" && sceneIndex <= totalSceneCount)
         {
             SceneManager.LoadScene(sceneIndex + 1);
         }
