e21ccade5a1664753fa29e9a29b79018a0a3d410
Stanisław Sklepowicz <stanislaw.sklepowicz@gmail.com>
2023-12-03 23:42:11 +0100

    Unstable platform zrobiony lepiej

--- Assets/Scripts/UnstablePlatform.cs
+++ Assets/Scripts/UnstablePlatform.cs
@@ -4,63 +4,105 @@ using UnityEngine;
 
 public class UnstablePlatform : MonoBehaviour
 {
-    //Color colour; //placeholder do animacji
-    BoxCollider2D colider;
-    private bool CRrunning;
-    //private int animationStage;
-    void Start()
-    {
-        colider = GetComponent<BoxCollider2D>();
-        //colour = GetComponent<Renderer>().material.color; //placeholder 2
-        //animationStage = 0;
-        colider.enabled = true;
-        //spriteRenderer.sprite = spriteStage1;
-    }
+    public PlatformStateSprites[] sprites;
+    [SerializeField] private PlatformStages currentStage;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (!CRrunning)
+        if (currentStage == PlatformStages.Idle)
         {
-            StartCoroutine(PlatformDestruction());
+            StartCoroutine(PlatformChangingCor());
         }
     }
 
-    private IEnumerator PlatformDestruction()
+    public void NextPlatformStage()
     {
-        CRrunning = true;
-
-        // stage 2
-        //animationStage++;
-        GetComponent<Renderer>().material.color = Color.blue;
-        yield return new WaitForSeconds(2);
-
-        // stage 3
-        //animationStage++;
-        GetComponent<Renderer>().material.color = Color.green;
-        yield return new WaitForSeconds(2);
-
-        // stage 4 - platform break
-        //animationStage++;
-        GetComponent<Renderer>().material.color = Color.cyan;
-        colider.enabled = false;
-        yield return new WaitForSeconds(4);
-
-        // stage 5 - platform respawn
-        GetComponent<Renderer>().material.color = Color.white;
-        //animationStage = 0;
-        colider.enabled = true;
-
-        CRrunning = false;
+        PlatformStages newState = currentStage + 1;
+        if (newState >= PlatformStages.count)
+        {
+            newState = PlatformStages.Idle;
+        }
+        PlatformChange(newState);
     }
-    //enum ze stagami
-    public enum PlatformStages 
-    { 
-        Idle,
-        Breaking1,
-        Breaking2,
-        Breaking3,
-        Breaking4,
-        Breaking5,
-        Gone
+
+    //Zmienia stany
+    private IEnumerator PlatformChangingCor()
+    {
+        NextPlatformStage();
+
+        yield return new WaitForSeconds(0.2f);
+        NextPlatformStage();
+
+        yield return new WaitForSeconds(0.2f);
+        NextPlatformStage();
+
+        yield return new WaitForSeconds(0.2f);
+        NextPlatformStage();
+
+        yield return new WaitForSeconds(0.2f);
+        NextPlatformStage();
+
+        yield return new WaitForSeconds(0.2f);
+        NextPlatformStage();
+
+        //recharge platformy i jej respawn
+        yield return new WaitForSeconds(3f);
+        NextPlatformStage();
     }
+
+    // Zmiana stagow
+    void PlatformChange(PlatformStages state)
+    {
+        currentStage = state;
+        DoStuffWithState();
+    }
+
+    private void DoStuffWithState()
+    {
+        switch (currentStage)
+        {
+            case PlatformStages.Idle:
+                {
+                    GetComponent<BoxCollider2D>().enabled = true;
+                    break;
+                }
+            case PlatformStages.Broken:
+                {
+                    GetComponent<BoxCollider2D>().enabled = false;
+                    break;
+                }
+        }
+
+        foreach(var sprite in sprites)
+        {
+            if(sprite.state == currentStage)
+            {
+                GetComponent<SpriteRenderer>().sprite = sprite.sprite;
+                break;
+            }
+        }
+    }
+}
+
+public enum PlatformStages 
+{ 
+    Idle,
+    Breaking1,
+    Breaking2,
+    Breaking3,
+    Breaking4,
+    Breaking5,
+    Broken,
+    //Respawn1,
+    //Respawn2,
+    //Respawn3,
+
+    count
+}
+
+[System.Serializable]
+public struct PlatformStateSprites
+{
+    public PlatformStages state;
+    public Sprite sprite;
 }
