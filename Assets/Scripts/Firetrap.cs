0efbc635379a24debfef27d66e6eecae972cf9f5
Stanisław Sklepowicz <stanislaw.sklepowicz@gmail.com>
2024-01-07 20:13:09 +0100

    Cheaty dodane. Upgrade scen do światła zrobiony. Pułapka ogniowa ma już światło

--- Assets/Scripts/Firetrap.cs
+++ Assets/Scripts/Firetrap.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class Firetrap : MonoBehaviour
 {
@@ -19,6 +20,13 @@ public class Firetrap : MonoBehaviour
     [SerializeField] Sprite sprite2;
     [SerializeField] Sprite sprite3;
     [SerializeField] Sprite sprite4;
+    
+    [Space(10)]
+
+    [SerializeField] Light2D lightSize1;
+    [SerializeField] Light2D lightSize2;
+    [SerializeField] Light2D lightSize3;
+    [SerializeField] Light2D lightSize4;
     private void Awake()
     {
         anim = GetComponent<Animator>();
@@ -53,18 +61,25 @@ public class Firetrap : MonoBehaviour
         boxCollider.enabled = true;
         burnSoundEffect.Play();
         spriteRend.sprite = sprite1;
+        lightSize1.enabled = true; //light level 1
         boxCollider.offset = new Vector2(0f, 0.6f);
         boxCollider.size = new Vector2(0.6f, 0.9f);
         yield return new WaitForSeconds(0.1f);
         spriteRend.sprite = sprite2;
+        lightSize1.enabled = false;
+        lightSize2.enabled = true;  //light level 2
         boxCollider.offset = new Vector2(0f, 1.5f);
         boxCollider.size = new Vector2(0.6f, 2.7f);
         yield return new WaitForSeconds(0.1f);
         spriteRend.sprite = sprite3;
+        lightSize2.enabled = false;
+        lightSize3.enabled = true;  //light level 3
         boxCollider.offset = new Vector2(0f, 2.22f);
         boxCollider.size = new Vector2(0.6f, 4f);
         yield return new WaitForSeconds(0.1f);
         spriteRend.sprite = sprite4;
+        lightSize3.enabled = false;
+        lightSize4.enabled = true;  //light level 4
         anim.enabled = true;
         boxCollider.size = new Vector2(0.6f, 6f);
         boxCollider.offset = new Vector2(0f, 3f);
@@ -73,6 +88,7 @@ public class Firetrap : MonoBehaviour
         exitSoundEffect.Play();
         anim.enabled = false;
         spriteRend.sprite = sprite2;
+        lightSize4.enabled = false;
         boxCollider.offset = new Vector2(0f, 1.5f);
         boxCollider.size = new Vector2(0.6f, 2.7f);
         yield return new WaitForSeconds(0.1f);
